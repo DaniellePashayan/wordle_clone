5ebed8187e365254f39e48eff12a351afe1b4d99
Danielle Pashayan <daniellepashayan@gmail.com>
2024-04-03 20:31:49 -0400

    updated main to call Game class

--- main.py
+++ main.py
@@ -1,44 +1,5 @@
-import pygame
-import sys
-import random
-
-from src.utility import get_word_list_from_file
-from src.settings import *
-
-
-def main():
-    def game_loop():
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            SCREEN.fill("white")
-            SCREEN.blit(BACKGROUND, BACKGROUND_RECT)
-            pygame.display.update()
-        
-    pygame.init()
-    
-    # sets up the window display
-    pygame.display.set_caption("PyWordle - DaniellePashayan")
-    SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-    ICON = pygame.image.load("assets/Icon.png")
-    pygame.display.set_icon(ICON)
-    
-    # loads the fonts
-    GUESSED_LETTER_FONT = pygame.font.Font("assets/FreeSansBold.otf", 50)
-    AVAILABLE_LETTER_FONT = pygame.font.Font("assets/FreeSansBold.otf", 25)
-    
-    # loads the background placeholder image, resizes, and centers it
-    BACKGROUND = pygame.image.load("assets/blank.png")
-    aspect_ratio = BACKGROUND.get_width() / BACKGROUND.get_height()
-    new_width = 400
-    new_height = new_width / aspect_ratio
-    BACKGROUND = pygame.transform.scale(BACKGROUND, (new_width, new_height))
-    BACKGROUND_RECT = BACKGROUND.get_rect(center=(WIDTH // 2, 300))
-
-    game_loop()
+from src.Game import Game
 
 if __name__ == "__main__":
-    main()
+    game = Game()
+    game.game_loop()
