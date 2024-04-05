321d50c0b2d5afd2deec1715e0ea55b8dcb95b71
Danielle Pashayan <daniellepashayan@gmail.com>
2024-04-05 18:44:40 -0400

    added func to select random word from list

--- src/Game.py
+++ src/Game.py
@@ -3,6 +3,7 @@ import sys
 import random
 from src.settings import *
 from src.Indicator import Indicator
+from src.utility import get_word_list
 
 class Game:
     def __init__(self):
@@ -39,10 +40,15 @@ class Game:
         self.indicators = []
 
         self.game_result = ""
+        
+        self.selected_word = self.select_random_word()
 
     def draw_keyboard(self):
         indicator_y = 550
-        starting_indicator_x = [55, 80, 130]
+        spacing_gap = 5
+        # the keyboard rows are different sizes due to num of letters in each row
+        # these numbers set the starting position of the letters in each row so that they are centered
+        starting_indicator_x = [75, 95, 140]
         # renders the rows of the keyboard
         for i in range(len(KEYBOARD_ALPHABET)):
             # renders the letters in each row
@@ -52,11 +58,17 @@ class Game:
                 self.indicators.append(new_ind)
 
                 # shifts the starting position of the letter on the x axis
-                indicator_x += INDICATOR_SIZE + 10
+                indicator_x += INDICATOR_SIZE_W + spacing_gap
                 new_ind.draw()
             # shifts the position of the letter on the y axis for each new row and resets the x axis
-            indicator_y += 50
-            
+            indicator_y += 55
+    
+    def select_random_word(self):
+        # selects a random word from the list of words
+        w = random.choice(get_word_list())
+        print(w)
+        return w
+    
             
     def game_loop(self):
         while True:
