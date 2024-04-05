82614a8215315c8951cc76ced7f4fcdb9acfbfb8
Danielle Pashayan <daniellepashayan@gmail.com>
2024-04-05 18:44:24 -0400

    changed the font color and spacing of keyboard

--- src/Indicator.py
+++ src/Indicator.py
@@ -7,7 +7,7 @@ class Indicator:
         self.x = x
         self.y = y
         self.letter = letter
-        self.rect = (self.x, self.y, INDICATOR_SIZE, INDICATOR_SIZE)
+        self.rect = (self.x, self.y, INDICATOR_SIZE_W, INDICATOR_SIZE_H)
         self.bg_color = LIGHT_GRAY
         self.screen = screen
 
@@ -15,7 +15,7 @@ class Indicator:
     def draw(self):
         # puts the placeholder and text on the screen at the desired location
         pygame.draw.rect(self.screen, pygame.Color(self.bg_color), self.rect)
-        self.text_surface = pygame.font.Font(FONT, LETTER_SIZE).render(self.letter, True, pygame.Color("white"))
-        self.text_rect = self.text_surface.get_rect(center=(self.x + INDICATOR_SIZE // 2, self.y + INDICATOR_SIZE // 2))
+        self.text_surface = pygame.font.Font(FONT, LETTER_SIZE).render(self.letter, True, pygame.Color("black"))
+        self.text_rect = self.text_surface.get_rect(center=(self.x + INDICATOR_SIZE_W // 2, self.y + INDICATOR_SIZE_H // 2))
         self.screen.blit(self.text_surface, self.text_rect)
         
